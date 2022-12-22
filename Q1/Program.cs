89e55284d6bca6f0486731fedba456a9d01bfc70
Rungaroon <rung.kmitl@gmail.com>
2022-12-22 16:26:14 +0700

    import code

---
+++ Q1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter value for print image:");
+            int inputVal = Convert.ToInt32(Console.ReadLine());
+            int realInput = inputVal + 1;
+            Console.WriteLine($"Number more than: {inputVal}({realInput})");
+            PrintImage(realInput);
+        }
+
+        public static void PrintImage(int inputVal)
+        {
+            Console.WriteLine($"Output: String");
+
+            int totalAsterisk = 1;
+            int numSpace = 0;
+            int totalNumber = 1;
+
+            for (int i = 1; i < inputVal - 1; i++)
+            {
+                totalNumber = totalNumber + 2;
+            }
+
+            for (int i = 1; i <= inputVal + 1; i++)
+            {
+                int totalSpace = totalNumber - numSpace;
+                for (int space = 0; space <= totalSpace; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int asterisk = 0; asterisk < totalAsterisk; asterisk++)
+                {
+                    Console.Write("* ");
+                }
+
+                totalAsterisk = totalAsterisk + 2;
+                numSpace = numSpace + 2;
+                if (totalSpace == 1)
+                {
+                    totalAsterisk = 1;
+                    numSpace = 0;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Q3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter gas liter:");
+            int inputVal = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Number:Gas Liter:{inputVal}");
+            CalculateDistance(inputVal);
+        }
+
+        public static void CalculateDistance(int inputVal)
+        {
+            int totalWheel = (inputVal * 13000);
+            int distanceCm = (totalWheel * 95);
+            int distanceMeter = distanceCm / 100;
+            int distanceKm = distanceMeter / 1000;
+
+            Console.WriteLine($"String: Distance ({distanceKm} KM) ");
+        }
+    }
+}
